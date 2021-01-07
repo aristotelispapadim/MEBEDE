7bbe21acfcc6dcabe62ace7dee53dbceb03cf690
aristotelispapadim <56359822+aristotelispapadim@users.noreply.github.com>
2021-01-07 13:44:45 +0200

    .

--- clark_n_wright.py
+++ clark_n_wright.py
@@ -53,59 +53,6 @@ for i in range(0, len(all_nodes)):
             time = 0
         time_matrix[i][j] = time
 
-time_matrixA = time_matrix.copy()
-
-
 total_nodes = []
 for i in range(1,201):
     total_nodes.append(i)
-
-
-trucks = {}
-for i in range (1,26):
-    trucks[i] = [[0],3000]
-
-#trucks[1] = [[0,8,9,6],2500]
-#print(trucks.get(1)[0][3])
-#trucks.get(1)[0].append(17)
-
-
-#για καθε γραμμη i βρισκω το ελαχιστο j (στηλη)
-def FindMin(a):
-    min = 100000
-    min_pos = 0
-    for j in range (1, len(all_nodes)):
-        if(time_matrix[a][j] < min and time_matrix[a][j] > 0):
-            min = time_matrix[a][j]
-            min_pos = j
-            minimums = [min, min_pos]
-    return minimums
-
-#pos = mins[1]
-#if(trucks.get(1)[1] >= all_nodes[pos].demand):
-  #  trucks.get(1)[0].append(pos)
-    #trucks.get(1)[1] -= all_nodes[pos].demand
-    #for i in range(0, len(all_nodes)):
-      #  time_matrixA[i][pos] = 0
-
-
-mins = []
-for i in range (1,26):
-    last_number = trucks.get(i)[0][(len(trucks.get(i)[0])-1)]
-    mins.append(FindMin(last_number))
-    pos = mins.get(i-1)[1]
-    if(trucks.get(i)[1] < all_nodes[pos].demand):
-        
-
-min_of_mins = 100000
-for i in range (0,25):
-    if(mins[i][0] < min_of_mins):
-        min_of_mins = mins[i][0]
-        pos_min = mins[i][1]
-        truck = i+1
-
-for i in range (1,26):
-    if(trucks.get(truck))
-if()
-
-print(min_of_mins)
