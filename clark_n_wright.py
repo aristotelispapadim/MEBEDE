6921b7955b59a12905dda746d8376622e1307fb9
aristotelispapadim <56359822+aristotelispapadim@users.noreply.github.com>
2021-01-07 16:33:30 +0200

    Update clark_n_wright.py

--- clark_n_wright.py
+++ clark_n_wright.py
@@ -39,20 +39,78 @@ for i in range(0, len(all_nodes)):
 
 time_matrix = [[0.0 for j in range(0, len(all_nodes))] for k in range(0, len(all_nodes))]
 for i in range(0, len(all_nodes)):
-    for j in range(1, len(all_nodes)):
+    for j in range(0, len(all_nodes)):
         if(i != j):
             time = dist_matrix [i][j] / 35
             if(all_nodes[j].type == 1):
                 time +=(5/60)
             elif(all_nodes[j].type == 2):
                 time +=(15/60)
-            else:
+            elif(all_nodes[j].type == 3):
                 time +=(25/60)
+
+                
         else:
             #χρονος μεγαλυτερος του 0
             time = 0
         time_matrix[i][j] = time
+#print(time_matrix[4][2])
 
 total_nodes = []
 for i in range(1,201):
     total_nodes.append(i)
+
+savings = [[0.0 for j in range(0, len(all_nodes))] for k in range(0, len(all_nodes))]
+for i in range(1, len(all_nodes)):
+    for j in range(1, len(all_nodes)):
+        if(i!=j):
+            savings[i][j] = time_matrix[i][0] + time_matrix[0][j] - time_matrix[i][j]
+
+
+dictionary = {}
+for i in range(1, len(all_nodes)):
+    for j in range(1, len(all_nodes)):
+        dictionary[(i,j)] = [savings[i][j]]
+#print(dictionary)
+
+sort_dict = sorted(dictionary.items(), key=lambda kv:(kv[1], kv[0]), reverse=True)
+  
+#sorted savings
+nodes=[]
+for i in range(0,len(sort_dict)):
+    nodes.append(list(sort_dict)[i][0])
+
+print(nodes[0][0])
+print(nodes[0][1])
+
+
+#if():
+ #   if((roads[nodes[0][0]][0][1] == nodes[0][0] or roads[nodes[0][0]][0][len(roads[nodes[0][0]][0])-2] == nodes[0][0]) and (roads[nodes[0][1]][0][1] == nodes[0][1] or roads[nodes[0][1]][0][len(roads[nodes[0][1]][0])-2] == nodes[0][1])):
+
+roads = []
+roads.append(000)
+for i in range (1,201):
+    roads.append([0,i,0])
+
+for i in range(1,201):
+    if nodes[0][0] in roads[i]:
+        pos1 = i
+print(pos1)
+
+for i in range(1,201):
+    if nodes[0][1] in roads[i]:
+        pos2 = i
+print(pos2)
+
+if(pos1!=pos2):
+    if((roads[pos1][1] == nodes[0][0] or roads[pos1][len(roads[pos1])-2] == nodes[0][0]) and (roads[pos2][1] == nodes[0][1] or roads[pos2][len(roads[pos2])-2] == nodes[0][1])):
+        if(all_nodes[pos1].demand + all_nodes[pos2].demand <= 3000):
+
+
+
+
+
+
+
+
+
