7dc4e67b257a1f44b8632f10737bf27e7c2a85cc
aristotelispapadim <56359822+aristotelispapadim@users.noreply.github.com>
2021-01-14 15:52:20 +0200

    .

--- clark_n_wright_version2.py
+++ clark_n_wright_version2.py
@@ -95,7 +95,7 @@ for k in range(0,len(sort_dict)):
                 sum1 += all_nodes[roads[pos1][i]].demand
             for i in range(1,len(roads[pos2])-1):
                 sum2 += all_nodes[roads[pos2][i]].demand
-            if((sum1 + sum2) <= 2800):
+            if((sum1 + sum2) <= 2300):
                 roads[pos1].insert(len(roads[pos1])-1,n1) #προσθετω στην πρωτη διαδρομη την δευτερη διαδρομη
                 roads[pos2]=[0,0,0]  # σβηνω τη διαδρομη που εχω προσθεσει
 
@@ -105,7 +105,6 @@ for i in range(1,201):
     if(roads[i]!=[0,0,0]):
         count+=1
         print(roads[i])
-        #print(roads[i])
         time_sum=0
         for j in range(0,len(roads[i])-2):
             if(all_nodes[roads[i][j+1]].type == 1):
@@ -116,9 +115,54 @@ for i in range(1,201):
                 time_out =(25/60)
             time_sum+=time_matrix[roads[i][j]][roads[i][j+1]]
             time_sum+=time_out
-        print(time_sum)
+        #print(time_sum)
         if(mymax<time_sum):
             mymax=time_sum
 
-print(count)
+#print(count)
 print(mymax)
+
+'''
+mylist = [[0,1,50,65,101,130,150,165,183],
+[0,2,46,74,77,96,120,146,179],
+[0,3,34,72,104,123,145,162,194],
+[0,4,40,80,103,138,163,175,198],
+[0,5,49,59,70,98,106,140,166,195],
+[0,6,36,62,91,107,131,178],
+[0,7,33,73,99,105,126,153,169],
+[0,8,47,78,115,143,167,193],
+[0,9,39,61,90,100,108,119,133,159,177,187],
+[0,10,51,75,85,102,127,154,173],
+[0,11,48,82,111,142,156,184],
+[0,12,30,41,69,93,125,151,164],
+[0,13,38,79,94,141,158,196,199],
+[0,14,29,60,88,117,135,155,180,200],
+[0,15,43,57,95,124,148,181,189],
+[0,16,52,87,122,160,192],
+[0,17,27,44,76,112,137,171,188],
+[0,18,31,56,64,86,116,149,174,185],
+[0,19,28,55,71,110,129,168],
+[0,20,32,58,84,114,147,170,191],
+[0,21,37,53,67,97,144,161,197],
+[0,22,35,81,118,136,157,190],
+[0,23,42,68,89,128,139,176,186],
+[0,24,54,92,113,134,152,182],
+[0,25,26,45,63,66,83,109,121,132,172]]   
+
+max1=-1
+for i in range(0,24):
+    newtime_sum=0
+    for j in range(0,len(mylist[i])-1):
+        if(all_nodes[mylist[i][j+1]].type == 1):
+            newtime_out =(5/60)
+        elif(all_nodes[mylist[i][j+1]].type == 2):
+            newtime_out =(15/60)
+        elif(all_nodes[mylist[i][j+1]].type == 3):
+            newtime_out =(25/60)
+        newtime_sum+=time_matrix[mylist[i][j]][mylist[i][j+1]]
+        newtime_sum+=newtime_out
+    #print(newtime_sum)
+    if(max1<newtime_sum):
+        max1=newtime_sum
+print(max1)
+'''
